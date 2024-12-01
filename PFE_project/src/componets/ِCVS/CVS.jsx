2713a271d7088f903f40b86db1eb9895fd5eef03
wassim <wslimani455@gmail.com>
2024-12-01 14:37:05 +0100

    done3

--- PFE_project/src/componets/ِCVS/CVS.jsx
+++ PFE_project/src/componets/ِCVS/CVS.jsx
@@ -53,7 +53,7 @@ function CVS() {
                   viewBox="0 0 24 24"
                   className="upload-icon"
                 >
-                  {/* <circle cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="2" fill="none" /> */}
+                  <circle cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="2" fill="none" />
                   <path
                     d="M12 8v8m0 0l4-4m-4 4l-4-4"
                     stroke="currentColor"
